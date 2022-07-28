8a55ab342f9ae72a62cea04a5eb3b7f405f09263
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-28 12:38:18 -0700

    added seed and working

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
+const petRoutes = require('./app/routes/pet_routes')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 
@@ -65,6 +66,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
+app.use(petRoutes)
 app.use(exampleRoutes)
 app.use(userRoutes)
 
